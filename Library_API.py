9a3bd57ccab1a9a53b3d2a704b5879eb6c47389c
Jyoti4020 <110450741+Jyoti4020@users.noreply.github.com>
2022-08-02 15:16:47 +0530

    update library

--- Library_API.py
+++ Library_API.py
@@ -86,39 +86,13 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key =true)
     name = db.Column(db.String(50))
     city = db.Column(db.String(50))
-    user = db.relationship('Subscription_users')
+    #user = db.relationship('Subscription_users')
 
     def __init__(self,id,name,city):
         self.id = id
         self.name = name
         self.city = city
         
-@dataclass        
-class Subscription_users(db.Model):
-    __tablename__='Subscription_users'
-    
-    id = int
-    library_id : int
-    user_id : int
-    
-    id = db.Column(db.Integer, primary_key =true)
-    library_id = db.Column(db.Integer, db.ForeignKey('Library.id'))
-    user_id = db.Column(db.Integer, db.ForeignKey('User.id'))
-   
-    def __init__(self,id,library_id,user_id):
-        self.id = id
-        self.library_id = library_id
-        self.user_id = user_id
-        
-## insert data in sub_user
-@app.route('/library/<int:lib_id>/<int:user_id>/subuser',methods = ["POST"])
-def insert_sub_user(lib_id,user_id):    
-    sub = request.get_json()
-    new_sub = Subscription_users(sub["id"],lib_id,user_id) 
-    db.session.add(new_sub)
-    db.session.commit()
-    return(" sub_user added ")
-
 
 @app.route('/library')
 ## get all library table data
@@ -126,56 +100,6 @@ def get_library():
     lib_data = Library.query.all()
     return jsonify(lib_data)
 
-## 1.get all users by one library
-@app.route('/library/<int:lib_id>/user')
-def get_lib_wise_user(lib_id):    
-    user_data = Subscription_users.query.filter_by(library_id = lib_id).all()
-    return jsonify(user_data)   
-## 2. get list of users_id in one library
-@app.route('/library/<int:lib_id>/user')
-def get_users(lib_id): 
-    user = {}
-    user_list = []
-    user_data = Subscription_users.query.filter_by(library_id = lib_id).with_entities(Subscription_users.user_id).all() 
-    for users in user_data :
-        user_list.append(users.user_id)           
-    user.update({"user_id " :user_list})
-    return jsonify(user)  
- 
-## 3.get list of user details in particular Library
-@app.route('/library/<int:lib_id>/user')
-def get_users(lib_id): 
-    user_dict = {}    
-    user_list = []
-    user_data = Subscription_users.query.filter_by(library_id = lib_id).with_entities(Subscription_users.user_id,User.id,User.name,User.city).all() 
-
-    for users in user_data :
-        user = {}
-        if users.user_id == users.id :            
-            user.update({"Id" : users.user_id})    
-            user.update({"name" : users.name})     
-            user.update({"city" : users.city}) 
-            user_list.append(user)       
-              
-    user_dict.update({"user Details " : user_list})
-    return jsonify(user_dict)   
- 
-## 1.get all library by one users
-@app.route('/user/<int:u_id>/library')
-def get_user_wise_lib(u_id):    
-    lib_user_data = Subscription_users.query.filter_by(user_id = u_id).all()
-    return jsonify(lib_user_data)    
-## 2. get list if library_id in one user
-@app.route('/user/<int:u_id>/library')
-def get_userid_lib(u_id): 
-    library = {}
-    lib_list = []
-    lib_user_data = Subscription_users.query.filter_by(user_id = u_id).with_entities(Subscription_users.library_id).all()
-    for lib in lib_user_data:
-        lib_list.append(lib.library_id)           
-    library.update({"library_id " : lib_list})
-    return jsonify(library)   
-      
 ## group by libraryId in dept
 # @app.route('/lib/dept/re')
 # def group_lib():   
